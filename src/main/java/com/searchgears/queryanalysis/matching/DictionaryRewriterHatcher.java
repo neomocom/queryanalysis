7da3d65d8dc78df77e4c32ef49413d9bef147211
Marc Weinberger <marc.weinberger@me.com>
2020-05-25 13:03:38 +0200

    load config and write synonyms file in DictionaryRewriterHatcher
    - generalize Config loading from classpath
    - remove duplicate test resources

--- src/main/java/com/searchgears/queryanalysis/matching/DictionaryRewriterHatcher.java
+++ src/main/java/com/searchgears/queryanalysis/matching/DictionaryRewriterHatcher.java
@@ -1,8 +1,11 @@
 package com.searchgears.queryanalysis.matching;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.searchgears.queryanalysis.config.Config;
 import com.searchgears.queryanalysis.config.Matcher;
 import org.apache.lucene.analysis.util.ResourceLoader;
 import org.apache.lucene.analysis.util.ResourceLoaderAware;
+import org.apache.solr.core.SolrResourceLoader;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -17,30 +20,59 @@ import java.util.Map;
  * created directly except for testing.
  * It is not a factory because it needs to go through the solr resource loader dance before it can actually
  * create the DictionaryRewriter.
+ * TODO rename to DictionaryRewriterHolder
  */
 public class DictionaryRewriterHatcher implements ResourceLoaderAware {
+    private static final String DEFAULT_SYN_FILENAME = "synonyms.txt";
+
+    private final String configFilename;
+    private final String synFilename;
+
     private DictionaryRewriter rewriter = null;
     private ResourceLoader loader;
 
-    public DictionaryRewriter getRewriter() {
-        if (rewriter == null) {
-            throw new UnsupportedOperationException("DictionaryRewriter is only available after ResourceLoaderAware#inform");
-        }
-        return rewriter;
+    public DictionaryRewriterHatcher(String configFilename) {
+        this(configFilename, DEFAULT_SYN_FILENAME);
+    }
+
+    public DictionaryRewriterHatcher(String configFilename, String synFilename) {
+        this.configFilename = configFilename;
+        this.synFilename = synFilename;
     }
 
     @Override
-    public void inform(ResourceLoader pLoader)  {
-        loader = pLoader;
-        //createDictionaryRewriterInputFile();
-        //createDictionaryRewriter();
-        //informDictionaryRewriter();
+    public void inform(ResourceLoader loader) throws IOException {
+        this.loader = loader;
+        Path synFilepath = resolvePathToSynonymsFile();
+        createSynFileForRewriter(synFilepath);
+        createDictionaryRewriter();
+
+        // TODO delete synFile after rewriter init
         //deleteDictionaryRewriterInputFile();
     }
 
-    public void createSynFileFromMatchers(Map<String, Matcher> matchers, Path outputFile) {
+    private Path resolvePathToSynonymsFile() {
+        if (loader instanceof SolrResourceLoader) {
+            SolrResourceLoader solrResourceLoader = ((SolrResourceLoader) loader);
+            return solrResourceLoader.getInstancePath().resolve("conf").resolve(synFilename);
+        } else {
+            throw new IllegalStateException("Solr resource loader is required to resolve path to solr core config dir");
+        }
+    }
+
+    private void createSynFileForRewriter(Path synFilepath) {
+        try (InputStream inputStream = loader.openResource(configFilename)) {
+            Config config = Config.fromInputStream(inputStream);
+            createSynFileFromMatchers(config.getMatchers(), synFilepath);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Config file could not be loaded");
+        }
+    }
+
+    @VisibleForTesting
+    void createSynFileFromMatchers(Map<String, Matcher> matchers, Path outputFile) {
         try (BufferedWriter output = Files.newBufferedWriter(outputFile, StandardCharsets.UTF_8)) {
-            for (Map.Entry<String, Matcher> matcher: matchers.entrySet()) {
+            for (Map.Entry<String, Matcher> matcher : matchers.entrySet()) {
                 writeDictionaryContents(matcher.getValue().getDictionary(), matcher.getKey(), output);
             }
         } catch (IOException e) {
@@ -49,7 +81,7 @@ public class DictionaryRewriterHatcher implements ResourceLoaderAware {
     }
 
     private void writeDictionaryContents(String dictionary, String matcherRule, BufferedWriter output) {
-        try (InputStream inputStream = loader.openResource(dictionary);) {
+        try (InputStream inputStream = loader.openResource(dictionary)) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
             String line;
             while ((line = reader.readLine()) != null) {
@@ -60,4 +92,17 @@ public class DictionaryRewriterHatcher implements ResourceLoaderAware {
             throw new IllegalArgumentException("Dictionary " + dictionary + " could not be opened. ");
         }
     }
+
+    private void createDictionaryRewriter() throws IOException {
+        this.rewriter = new DictionaryRewriter(synFilename);
+        this.rewriter.inform(loader);
+    }
+
+    public DictionaryRewriter getRewriter() {
+        if (rewriter == null) {
+            throw new UnsupportedOperationException("DictionaryRewriter is only available after ResourceLoaderAware#inform");
+        }
+        return rewriter;
+    }
+
 }
--- src/test/java/com/searchgears/queryanalysis/matching/DictionaryRewriterHatcherTest.java
+++ src/test/java/com/searchgears/queryanalysis/matching/DictionaryRewriterHatcherTest.java
@@ -3,17 +3,14 @@ package com.searchgears.queryanalysis.matching;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.searchgears.queryanalysis.SolrCoreAwareTest;
-import com.searchgears.queryanalysis.config.Config;
-import com.searchgears.queryanalysis.config.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.Before;
 import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -21,50 +18,69 @@ import static org.hamcrest.CoreMatchers.containsString;
 public class DictionaryRewriterHatcherTest extends SolrCoreAwareTest {
     @Rule
     public ExpectedException exception = ExpectedException.none();
-    private Config config;
     private DictionaryRewriterHatcher hatcher;
 
     @Before
-    public void readConfig() throws IOException {
-        config = parseConfig("queryanalysis.yml");
-        hatcher = new DictionaryRewriterHatcher();
-        hatcher.inform(getResourceLoader());
+    public void setUpTestSubject() {
+        hatcher = new DictionaryRewriterHatcher("queryanalysis.yml");
     }
 
     @Test
-    public void synonymFileIsCreatedForAllMatchers() throws IOException {
-        Path outputFile = createTempOutputFile();
-        hatcher.createSynFileFromMatchers(config.getMatchers(), outputFile);
+    public void informCreatesSynonymsFile() throws IOException {
+        hatcher.inform(getResourceLoader());
+        assertTrue(resolveSynFilepath().toFile().exists());
+    }
+
+    @Test
+    public void synFileNameCanBeCustomized() throws IOException {
+        String synFilename = "custom-synonyms.txt";
+        new DictionaryRewriterHatcher("queryanalysis.yml", synFilename).inform(getResourceLoader());
+        assertTrue(resolveSynFilepath(synFilename).toFile().exists());
+    }
+
+    @Test
+    public void synonymFileIsCreatedForAllMatcherDictionaries() throws IOException {
+        hatcher.inform(getResourceLoader());
         Set<String> expected = ImmutableSet.of(
                 "Verlag GmbH => publisherMarker", "e. V. => publisherMarker",
                 "Peter Suhrkamp => publisher", "Beck => publisher", "Hänssler => publisher"
         );
-        Set<String> actual = ImmutableSet.copyOf(Files.readAllLines(outputFile));
+        Set<String> actual = ImmutableSet.copyOf(Files.readAllLines(resolveSynFilepath()));
         assertEquals(expected, actual);
     }
 
     @Test
-    public void illegalDictionaryThrowsException() {
-        String invalidDictionary = "invalidDictionary.dic";
+    public void illegalDictionaryThrowsException() throws IOException {
         exception.expect(IllegalArgumentException.class);
-        exception.expectMessage(containsString(invalidDictionary));
-        Map<String, Matcher> matchers = ImmutableMap.of("publisher", new Matcher(invalidDictionary));
-        hatcher.createSynFileFromMatchers(matchers, createTempOutputFile());
+        exception.expectMessage(containsString("invalidDictionary.dic"));
+        new DictionaryRewriterHatcher("queryanalysis-invalid-dic.yml").inform(getResourceLoader());
     }
 
     @Test
     public void nonWritableOutputFileThrowsException() {
         exception.expect(IllegalStateException.class);
         exception.expectMessage(containsString("Could not write to output file"));
-        hatcher.createSynFileFromMatchers(config.getMatchers(), createReadOnlyOutputFile());
+        hatcher.createSynFileFromMatchers(ImmutableMap.of(), createReadOnlyOutputFile());
     }
 
+    @Test
+    public void invalidConfigFileLocationThrowsException() throws IOException {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage(containsString("Config file could not be loaded"));
+        new DictionaryRewriterHatcher("invalid-location").inform(getResourceLoader());
+    }
 
+    @Test
+    public void retrievalOfRewriterBeforeInformThrowsException() {
+        exception.expect(UnsupportedOperationException.class);
+        exception.expectMessage(containsString("only available after ResourceLoaderAware#inform"));
+        hatcher.getRewriter();
+    }
 
-    private Config parseConfig(String fileName) {
-        String file = ClassLoader.getSystemClassLoader()
-                .getResource(fileName).getFile();
-        return Config.fromFile(file);
+    @Test
+    public void informCreatesInstanceOfRewriter() throws IOException {
+        hatcher.inform(getResourceLoader());
+        assertNotNull(hatcher.getRewriter());
     }
 
     private Path createTempOutputFile() {
@@ -81,5 +97,13 @@ public class DictionaryRewriterHatcherTest extends SolrCoreAwareTest {
         Path outputFile = createTempOutputFile();
         outputFile.toFile().setReadOnly();
         return outputFile;
+    }
+
+    private Path resolveSynFilepath() {
+        return resolveSynFilepath("synonyms.txt");
+    }
+
+    private Path resolveSynFilepath(String synFilename) {
+        return getResourceLoader().getInstancePath().resolve("conf").resolve(synFilename);
     }
 }

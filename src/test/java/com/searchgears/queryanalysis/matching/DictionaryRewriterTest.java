60868ee03a59cb3e7f0c1957a12be5b11fa991e6
Marc Weinberger <marc.weinberger@me.com>
2020-05-27 11:44:24 +0200

    simplify initialization of DictionaryRewriter / Hatcher

--- src/test/java/com/searchgears/queryanalysis/matching/DictionaryRewriterTest.java
+++ src/test/java/com/searchgears/queryanalysis/matching/DictionaryRewriterTest.java
@@ -4,17 +4,13 @@ import com.searchgears.queryanalysis.SolrCoreAwareTest;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-
 
 public class DictionaryRewriterTest extends SolrCoreAwareTest {
     private DictionaryRewriter rewriter;
 
-
     @Before
-    public void initalizeRewriter() throws IOException {
-        rewriter = new DictionaryRewriter("synonyms.txt");
-        rewriter.inform(getResourceLoader());
+    public void initalizeRewriter() {
+        rewriter = new DictionaryRewriter("synonyms.txt", getResourceLoader());
     }
 
     @Test
@@ -30,12 +26,12 @@ public class DictionaryRewriterTest extends SolrCoreAwareTest {
     @Test
     public void textContainingMatchIsPartiallyRewritten() {
         assertEquals("this is returned as is publisher publisherMarker so is this",
-                rewriter.rewrite("this is returned as is, Suhrkamp Verlag so is this"));
+            rewriter.rewrite("this is returned as is, Suhrkamp Verlag so is this"));
     }
 
     @Test
     public void caseIsIgnored() {
         assertEquals("publisher publisherMarker",
-                rewriter.rewrite("sUhrkamp VERLAG"));
+            rewriter.rewrite("sUhrkamp VERLAG"));
     }
 }
